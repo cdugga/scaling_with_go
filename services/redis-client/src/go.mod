138c2d88b048175d1a17ec082b99bc5b140a614a
Colin Duggan <duggan.colin@gmail.com>
2021-03-13 21:59:33 +0000

    Testing redis connection

--- services/redis-client/src/go.mod
+++ services/redis-client/src/go.mod
@@ -1,3 +1,5 @@
-module github.com/cdugga/scaling_with_go/server
+module github.com/cdugga/scaling_with_go/redisclient
 
 go 1.16
+
+require github.com/go-redis/redis/v8 v8.7.1
--- services/redis-client/src/main.go
+++ services/redis-client/src/main.go
@@ -1,13 +1,38 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/go-redis/redis/v8"
 	"log"
 	"net/http"
 )
 
 func main(){
 	StartServer()
+	RedisConnection()
+
+}
+
+var ctx = context.Background()
+
+func RedisConnection(){
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     "redis-master:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	err := rdb.Set(ctx, "key", "value", 0).Err()
+	if err != nil {
+		panic(err)
+	}
+
+	val, err := rdb.Get(ctx, "key").Result()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("key", val)
 }
 
 func StartServer() {
