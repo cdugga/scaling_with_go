3e3419f968b5edfc38ef97d20cb50d48c6441292
Colin Duggan <duggan.colin@gmail.com>
2021-03-14 20:29:58 +0000

    Logic to fetch key value from redis datastore

--- services/redis-client/src/main.go
+++ services/redis-client/src/main.go
@@ -4,33 +4,40 @@ import (
 	"context"
 	"fmt"
 	"github.com/go-redis/redis/v8"
+	gohandlers "github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
 	"time"
-	gohandlers "github.com/gorilla/handlers"
-	"github.com/gorilla/mux"
+)
 
+
+const (
+	PORT=8081
 )
 
 func main(){
-	RedisConnection()
+	RedisTest()
 	StartServer()
-
-
 }
 
 var ctx = context.Background()
 
-func RedisConnection(){
-
-	log.Println("Connecting to Redis")
-	rdb := redis.NewClient(&redis.Options{
+func RedisConnection() *redis.Client {
+	return redis.NewClient(&redis.Options{
 		Addr:     "redis-master:6379",
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
+}
+
+
+func RedisTest(){
+
+	log.Println("Connecting to Redis")
+	rdb := RedisConnection()
 
 	err := rdb.Set(ctx, "key", "value", 0).Err()
 	if err != nil {
@@ -44,23 +51,50 @@ func RedisConnection(){
 	fmt.Println("key", val)
 }
 
-func Handler(w http.ResponseWriter, r *http.Request) {
+func AddKeyHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	fmt.Fprint(w, "Hello from the other side", r.URL.Path)
 }
 
+
+func FetchKeyHandler(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+
+	key, err := vars["key"]
+
+	if err != true {
+		http.Error(w, "Invalid request", http.StatusBadRequest)
+	}
+
+	log.Printf("Fetching key %s" , key)
+
+	val, err1 := RedisConnection().Get(ctx, key).Result()
+	if err1 != nil {
+		panic(err1)
+	}
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Requested value ", val)
+}
+
 func StartServer() {
 
+	log.Printf("Staring server on port %d ", PORT )
 	sm := mux.NewRouter()
 
 	getRouter := sm.Methods(http.MethodGet).Subrouter()
-	getRouter.HandleFunc("/", Handler)
+	getRouter.HandleFunc("/", AddKeyHandler)
+
+
+	getKeyRouter := sm.Methods(http.MethodGet).Subrouter()
+	getKeyRouter.HandleFunc("/value/{key}", FetchKeyHandler)
 
 	//CORS header
 	ch := gohandlers.CORS(gohandlers.AllowedOrigins([]string{"http://localhost:3000"}))
 
 	s := http.Server{
-		Addr: ":8081",
+		Addr: fmt.Sprint(":" , PORT),
 		Handler: ch(sm),
 		IdleTimeout: 120*time.Second,
 		ReadTimeout: 5 *time.Second,
@@ -84,9 +118,6 @@ func StartServer() {
 	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
 
 	s.Shutdown( tc)
-
-	//http.HandleFunc("/", Handler)
-	//log.Fatal(http.ListenAndServe(":8081", nil))
 
 }
 
